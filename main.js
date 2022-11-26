9261074ca288cd001988da6195caca84b954bb23
Andrei Ferrari <andrei.domingos@estudante.ifms.edu.br>
2022-11-25 21:23:28 -0400

    🎉 Finalizando Etapa de Busca

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import '@picocss/pico'
 import './style.css'
 
-const formConsultarUsuario = document.querySelector('#consultarUsuario')
+const formConsultarUsuario = document.querySelector('.consultarUsuario')
 const inputUsuario = formConsultarUsuario.usuario
-const informacoes = document.querySelector('#informacoes');
-const btnConsultarUsuario = document.querySelector('#btnConsultarUsuario')
+const informacoes = document.querySelector('.informacoes');
+const btnConsultarUsuario = document.querySelector('.btnConsultarUsuario')
 
 formConsultarUsuario.addEventListener('submit', function (event) {
   event.preventDefault();
@@ -14,18 +14,31 @@ formConsultarUsuario.addEventListener('submit', function (event) {
 })
 
 async function consultarUsuario(usuario) {
-  let response = await fetch(`https://api.github.com/users/${usuario}/json/`)
+  let response = await fetch(`https://api.github.com/users/${usuario}`)
   let dadosUsuario = await response.json();
 
   if (dadosUsuario.erro) {
     informacoes.innerHTML = `<div class="erro">Usuario não encontrado!</div>`
   } else {
     informacoes.innerHTML = `
-      <p>Foto: ${dadosUsuario.avatar_url}</p>
-        <p>Nome ${dadosUsuario.login}</p>
-        <p>Localidade ${dadosUsuario.url}</p>
+      <p>Foto: <img src="${dadosUsuario.avatar_url}"></p>
+        <p>Nome: ${dadosUsuario.name}</p>
+        <a href="${dadosUsuario.html_url}">Perfil no GitHub</a>  
       `
 
   }
   ativaLoader(false)
+}
+
+function ativaLoader(ativo){
+  if(ativo){
+      btnConsultarUsuario.setAttribute('aria-busy', 'true');
+      btnConsultarUsuario.textContent = 'Localizando Perfil, Aguarde...'
+  }else{
+      btnConsultarUsuario.removeAttribute('aria-busy');
+      btnConsultarUsuario.textContent = 'Consultar'
+  }
+
+ /*  ativaLoader(false) */
+  
 }
